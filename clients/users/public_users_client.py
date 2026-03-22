1a4035ad0e662d074843baf04320e518396fd0d1
MikeDragonCode <mikhailneronovqa@gmail.com>
2026-03-22 15:34:03 +0300

    Знакомство с API клиентами.Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,24 @@
+from httpx import Response
+from clients.api_client import ApiClient
+from typing import TypedDict
+
+class CreateUserRequestDict(TypedDict):
+  """
+        Описание структуры запроса для создания пользователя.
+        """
+  email: str
+  password: str
+  lastName: str
+  firstName: str
+  middleName: str
+
+class PublicUsersClient(ApiClient):
+
+    def create_user_api(self, request:CreateUserRequestDict) -> Response:
+        """
+                       Метод выполняет cоздание пользователя.
+                       :param request: Словарь с email, password, lastName, firstName, middleName
+                       :return: Ответ от сервера в виде объекта httpx.Response
+                       """
+        return self.post('/api/v1/users',json=request)
+
